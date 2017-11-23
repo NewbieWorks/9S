aa5307e60b8c1ad07ba7142b3303510f28b5bffb
Nathanael <evo220999@gmail.com>
2017-11-23 17:40:19 +0700

    'kitchen'

--- app.py
+++ app.py
@@ -45,22 +45,22 @@ line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
-##@app.route("/callback", methods=['POST'])
-##def callback():
-##    # get X-Line-Signature header value
-##    signature = request.headers['X-Line-Signature']
-##
-##    # get request body as text
-##    body = request.get_data(as_text=True)
-##    app.logger.info("Request body: " + body)
-##
-##    # handle webhook body
-##    try:
-##        handler.handle(body, signature)
-##    except InvalidSignatureError:
-##        abort(400)
-##
-##    return 'OK'
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
