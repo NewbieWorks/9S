0a8456c60f9d116651b766d897d11782e0bbaab3
NewbieWorks <evo220999@gmail.com>
2018-05-23 09:49:48 -0700

    7

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-
 from __future__ import unicode_literals
 
 import errno
@@ -198,14 +183,9 @@ def handle_text_message(event):
         line_bot_api.reply_message(event.reply_token, template_message)
     elif text == 'imagemap':
         pass
-    elif text == 'YoRHa' :
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='For the Glory of Mankind'))
-    elif text == '@emi[L]' :
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Summoned...'))
-    elif text.lower() == ('hai' or 'hello' or 'pagi' or 'pagii' or 'malam' or 'siang') or ('selamat' in text.lower()) :
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text + 'juga :D'))
-	elif text == '7 First Kisses' :
-		tosend = '''[SEVEN FIRST KISSES]
+    elif text == '7 First Kisses':
+        toString = '''
+[SEVEN FIRST KISSES]
 Link Sharing by 9S
 
 Episode 1 : https://www.youtube.com/watch?v=PUBrXIikG9k
@@ -218,7 +198,13 @@ Episode 7 : https://www.youtube.com/watch?v=qXGf2VAHw28
 Episode 8 : https://www.youtube.com/watch?v=jv37Kt5u8WI
 
 ~ End at Eps 8 ~'''
-		line_bot_api.reply_message(event.reply_token, TextSendMessage(text=tosend))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=toString))        
+    elif text == 'YoRHa' :
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='For the Glory of Mankind'))
+    elif text == '@emi[L]' :
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Summoned...'))
+    elif text.lower() == ('hai' or 'hello' or 'pagi' or 'pagii' or 'malam' or 'siang') or ('selamat' in text.lower()) :
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text + 'juga :D'))
     elif text == 'info' :
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='still in development, here\'s some command :\n \
                                                                       profile\n\
